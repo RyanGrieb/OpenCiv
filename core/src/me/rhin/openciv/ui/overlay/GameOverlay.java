9c51dfd11eb555e89860c3381a62de0b920b0a24
rhin123 <ryanrhin@gmail.com>
2020-06-30 13:44:29 -0500

    GameOverlay: Move FPS label to a suitable location

--- core/src/me/rhin/openciv/ui/overlay/GameOverlay.java
+++ core/src/me/rhin/openciv/ui/overlay/GameOverlay.java
@@ -4,19 +4,20 @@ import me.rhin.openciv.ui.game.StatusBar;
 import me.rhin.openciv.ui.label.CustomLabel;
 
 public class GameOverlay extends Overlay {
-	
+
 	public static final int HEIGHT = 20;
-	
+
 	private CustomLabel fpsLabel;
 	private StatusBar statusBar;
 
 	public GameOverlay() {
-
-		this.fpsLabel = new CustomLabel("FPS: 0", 4, 4, viewport.getWorldWidth(), 20);
-		this.addActor(fpsLabel);
-
 		this.statusBar = new StatusBar(0, viewport.getWorldHeight() - HEIGHT, viewport.getWorldWidth(), HEIGHT);
 		addActor(statusBar);
+
+		this.fpsLabel = new CustomLabel("FPS: 60.0");
+		fpsLabel.setPosition(viewport.getWorldWidth() - fpsLabel.getWidth() - 4,
+				viewport.getWorldHeight() - fpsLabel.getHeight() - 5);
+		this.addActor(fpsLabel);
 	}
 
 	public CustomLabel getFPSLabel() {
