f75af6b3df7b38a6d226becf0233ddb99d54f71b
rhin123 <ryanrhin@gmail.com>
2021-07-01 18:22:11 -0500

    CivType: Solve certain civs not being randomly chosen

--- server/src/me/rhin/openciv/server/game/civilization/CivType.java
+++ server/src/me/rhin/openciv/server/game/civilization/CivType.java
@@ -42,7 +42,7 @@ public enum CivType {
 		int max = CivType.values().length - 1;
 		int min = 1;
 		// CivType civType = CivType.values()[rnd.nextInt(max - min + 1) + min];
-		CivType civType = CivType.values()[rnd.nextInt(CivType.values().length - 2) + 1];
+		CivType civType = CivType.values()[rnd.nextInt(CivType.values().length - 1) + 1];
 		return civType;
 	}
 
