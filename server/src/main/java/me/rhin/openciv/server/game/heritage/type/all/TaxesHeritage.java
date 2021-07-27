eb0eb4159f410ba61cfffb74fb6e7dfd3046939a
rhin123 <ryanrhin@gmail.com>
2021-07-26 20:37:04 -0500

    TaxesHeritage: Implement taxes heritage

--- server/src/main/java/me/rhin/openciv/server/game/heritage/type/all/TaxesHeritage.java
+++ server/src/main/java/me/rhin/openciv/server/game/heritage/type/all/TaxesHeritage.java
@@ -1,12 +1,20 @@
 package me.rhin.openciv.server.game.heritage.type.all;
 
+import me.rhin.openciv.server.Server;
 import me.rhin.openciv.server.game.Player;
+import me.rhin.openciv.server.game.city.City;
 import me.rhin.openciv.server.game.heritage.Heritage;
+import me.rhin.openciv.server.listener.CityGrowthListener;
+import me.rhin.openciv.server.listener.CityStarveListener;
+import me.rhin.openciv.shared.stat.Stat;
 
-public class TaxesHeritage extends Heritage {
+public class TaxesHeritage extends Heritage implements CityGrowthListener, CityStarveListener {
 
 	public TaxesHeritage(Player player) {
 		super(player);
+
+		Server.getInstance().getEventManager().addListener(CityGrowthListener.class, this);
+		Server.getInstance().getEventManager().addListener(CityStarveListener.class, this);
 	}
 
 	@Override
@@ -26,7 +34,26 @@ public class TaxesHeritage extends Heritage {
 
 	@Override
 	protected void onStudied() {
-		//Look at library building
+		for (City city : player.getOwnedCities())
+			for (int i = 0; i < city.getStatLine().getStatValue(Stat.POPULATION); i++) {
+				city.getStatLine().addValue(Stat.GOLD_GAIN, 0.5F);
+			}
+	}
+
+	@Override
+	public void onCityStarve(City city) {
+		if (!isStudied() || !player.getOwnedCities().contains(city))
+			return;
+
+		city.getStatLine().subValue(Stat.GOLD_GAIN, 0.5F);
+	}
+
+	@Override
+	public void onCityGrowth(City city) {
+		if (!isStudied() || !player.getOwnedCities().contains(city))
+			return;
+
+		city.getStatLine().addValue(Stat.GOLD_GAIN, 0.5F);
 	}
 
 }
