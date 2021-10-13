54e036b55053b74130b2015cc144e5faa2748c3a
rhin123 <ryanrhin@gmail.com>
2021-10-13 13:57:47 -0500

    NextTurnWindow: Stop resize listener form being removed

--- core/src/main/java/me/rhin/openciv/ui/window/type/NextTurnWindow.java
+++ core/src/main/java/me/rhin/openciv/ui/window/type/NextTurnWindow.java
@@ -60,14 +60,15 @@ public class NextTurnWindow extends AbstractWindow implements RequestEndTurnList
 	public void onResize(int width, int height) {
 		super.setPosition(width / 2 - 200 / 2, 0);
 
-		//nextTurnButton.setPosition(getWidth() / 2 - 125 / 2, 4);
+		nextTurnButton.setPosition(getWidth() / 2 - 125 / 2, 4);
+		waitingNextTurnButton.setPosition(getWidth() / 2 - 125 / 2, 4);
 	}
 
 	@Override
 	public void onClose() {
 		super.onClose();
 
-		Civilization.getInstance().getEventManager().clearListenersFromObject(this);
+		// Civilization.getInstance().getEventManager().clearListenersFromObject(this);
 	}
 
 	@Override
