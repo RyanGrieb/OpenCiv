80ce32ab560075ed943d05863bd0947d5117bb01
rhin123 <ryanrhin@gmail.com>
2022-02-25 16:07:45 -0600

    Meta: Test webhook commit

--- core/src/main/java/me/rhin/openciv/ui/button/type/PreviousScreenButton.java
+++ core/src/main/java/me/rhin/openciv/ui/button/type/PreviousScreenButton.java
@@ -2,7 +2,6 @@ package me.rhin.openciv.ui.button.type;
 
 import me.rhin.openciv.Civilization;
 import me.rhin.openciv.ui.button.Button;
-import me.rhin.openciv.ui.screen.ScreenEnum;
 
 public class PreviousScreenButton extends Button {
 
