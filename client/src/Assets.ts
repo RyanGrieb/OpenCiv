c894b32a7fca571d55c53f30a64e082fc514ecb8
unknown <ryanrhin@gmail.com>
2023-03-12 15:59:23 -0500

    Meta: Rename all typescript files to PascalCase
    Meta: Fix last commit

---
+++ client/src/Assets.ts
@@ -0,0 +1,53 @@
+//TODO: Have different objects/functions for sounds,spritesheets,images.
+//TODO: Preload all images into textures
+//TODO: Handle spritesheet into textures
+//TODO: Preload all sounds
+
+export const spritehseetSize = 20; //20x20
+
+export enum GameImage {
+  BUTTON,
+  BUTTON_HOVERED,
+  SPRITESHEET,
+}
+
+export enum SpriteRegion {
+  ARCHER = "0,0",
+  BUILDER = "1,0",
+  CAMEL_ARCHER = "2,0",
+  CARAVAN = "3,0",
+  CATAPULT = "5,0",
+  COMPOSITE_BOWMAN = "7,0",
+  CROSSBOWMAN = "8,0",
+  HORSEMAN = "10,0",
+  ROMAN_LEGION = "11,0",
+  OCEAN = "16,6",
+  FRESHWATER = "7,7",
+  GRASS = "3,6",
+  GRASS_HILL = "4,6",
+  MOUNTAIN = "14,6",
+  DESERT = "10,5",
+  DESERT_HILL = "11,5",
+  PLAINS = "1,7",
+  PLAINS_HILL = "2,7",
+  TUNDRA = "15,7",
+  TUNDRA_HILL = "16,7",
+  SNOW = "3,8",
+  SNOW_HILL = "4,8",
+  JUNGLE = "10,6",
+  FOREST = "17,5",
+  CATTLE = "1,5",
+  SHEEP = "8,7",
+  FISH = "14,5",
+  HORSES = "6,6",
+  COPPER = "4,5",
+  STAR = "0,3",
+}
+
+export const assetList = [
+  require("../assets/images/ui_button.png"),
+  require("../assets/images/ui_button_hovered.png"),
+  require("../assets/images/spritesheet.png"),
+  require("../assets/images/font.png"),
+  require("../assets/images/logo.png"),
+];
---
+++ client/src/network/Client.ts
@@ -0,0 +1,125 @@
+export class CallbackData {
+  public callbackFunction: Function;
+  public globalEvent: boolean; // Not associated with the current scene.
+
+  constructor(callbackFunctions: Function, globalEvent: boolean) {
+    this.callbackFunction = callbackFunctions;
+    this.globalEvent = globalEvent;
+  }
+}
+
+export interface OnNetworkEventOptions {
+  eventName: string;
+  callback: (data: JSON) => void;
+  globalEvent?: boolean;
+}
+
+export class NetworkEvents {
+  private static storedEvents: Map<string, CallbackData[]>;
+
+  private constructor() {}
+
+  public static call(eventName: string, data: JSON) {
+    if (this.storedEvents.has(eventName)) {
+      //Call the stored callback function
+      const callbackDataList = this.storedEvents.get(eventName);
+      for (let callbackData of callbackDataList) {
+        callbackData.callbackFunction(data);
+      }
+    }
+  }
+
+  /**
+   * Register a callback function to be called when a network event is received.
+   *
+   * @param {OnNetworkEventOptions} options - Options for the event listener.
+   * @param {string} options.eventName - The name of the event to listen for.
+   * @param {(data: JSON) => void} options.callback - The callback function to be called when the event is received.
+   * @param {boolean} [options.globalEvent=false] - Determine if we don't remove the event when the scene changes.
+   */
+  public static on(options: OnNetworkEventOptions) {
+    if (!this.storedEvents) {
+      this.storedEvents = new Map<string, CallbackData[]>();
+    }
+    this.addCallbackEvent(
+      this.storedEvents,
+      options.eventName,
+      options.callback,
+      options.globalEvent
+    );
+  }
+
+  /**
+   * Removes all associated callback functions that isn't a globalEvent
+   */
+  public static clear() {
+    const globalEventCallbacks = this.getGlobalEventCallbacks(this.storedEvents);
+    this.storedEvents = globalEventCallbacks;
+  }
+
+  private static getGlobalEventCallbacks(storedEvents: Map<string, CallbackData[]>) {
+    const globalEventCallbacks = new Map<string, CallbackData[]>();
+
+    this.storedEvents.forEach((callbackDataList, eventName) => {
+      for (const callbackData of callbackDataList) {
+        if (callbackData.globalEvent) {
+          this.addCallbackEvent(
+            globalEventCallbacks,
+            eventName,
+            callbackData.callbackFunction,
+            true
+          );
+        }
+      }
+    });
+
+    return globalEventCallbacks;
+  }
+
+  private static addCallbackEvent(
+    storedEvents: Map<string, CallbackData[]>,
+    eventName: string,
+    callback: Function,
+    globalEvent?: boolean
+  ) {
+    //Get the list of stored callback functions or an empty list
+    let callbackDataList: CallbackData[] = storedEvents.get(eventName) ?? [];
+    // Append the to functions
+    callbackDataList.push(new CallbackData(callback, globalEvent));
+    storedEvents.set(eventName, callbackDataList);
+  }
+}
+
+export class WebsocketClient {
+  private static websocket: WebSocket;
+
+  private constructor() {}
+  // TODO: Add network events here with string & function that has arguments.
+  // e.g. setScreen & screenType arg.
+
+  public static init(serverAddress: string) {
+    this.websocket = new WebSocket("ws://" + serverAddress + ":2000/");
+
+    this.websocket.addEventListener("open", (event) => {
+      console.log("Connected to server");
+    });
+
+    this.websocket.addEventListener("message", (event) => {
+      console.log("Message from server: " + event.data);
+      const eventJSON = JSON.parse(event.data);
+      NetworkEvents.call(eventJSON["event"], eventJSON);
+    });
+
+    this.websocket.addEventListener("close", (event) => {
+      NetworkEvents.call("connectionClosed", JSON.parse("{}"));
+    });
+  }
+
+  public static disconnect() {
+    this.websocket.close();
+  }
+
+  public static sendMessage(message: Record<string, any>) {
+    this.websocket.send(JSON.stringify(message));
+  }
+}
