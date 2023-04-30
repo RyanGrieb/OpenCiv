2612df9b8fb954cb33d61a2c75bc736ca944f2dc
unknown <ryanrhin@gmail.com>
2023-04-30 11:50:21 -0500

    Server Game: Add documentation

--- server/src/Game.ts
+++ server/src/Game.ts
@@ -3,15 +3,22 @@ import { State } from "./state/State";
 import { WebSocket } from "ws";
 import { ServerEvents } from "./Events";
 
+/**
+ * Game class is responsible for managing the state of the game, and players.
+ */
 export class Game {
   private constructor() {}
   private static currentState: State;
   private static states: Map<string, State>;
   private static players: Map<string, Player>;
 
+  /**
+   * Initializes the game by setting up server event listeners for various events.
+   */
   public static init() {
     this.states = new Map<string, State>();
 
+    // Set up the listener for the "setState" event. Changes the game-state.
     ServerEvents.on({
       eventName: "setState",
       callback: (data: JSON) => {
@@ -20,12 +27,15 @@ export class Game {
       globalEvent: true,
     });
 
+    // Set up the listener for the "playerNames" event. Return all connected players in-game.
     ServerEvents.on({
       eventName: "playerNames",
       callback: (data: JSON, websocket) => {
+        // Get all player names and the name of the requesting player.
         const playerNames = Array.from(this.players.keys());
         const requestingPlayerName =
           this.getPlayerFromWebsocket(websocket)?.getName();
+        // Send the names to the requesting player.
         websocket.send(
           JSON.stringify({
             event: "playerNames",
@@ -37,9 +47,11 @@ export class Game {
       globalEvent: true,
     });
 
+    // Set up the listener for the "playerQuit" event.
     ServerEvents.on({
       eventName: "playerQuit",
       callback: (data: JSON) => {
+        // If only one player is remaining, set the state to "lobby".
         if (this.players.size <= 1) {
           Game.setState("lobby");
         }
@@ -48,10 +60,19 @@ export class Game {
     });
   }
 
+  /**
+   * Adds a state to the states map.
+   * @param stateName - The name of the state.
+   * @param state - The state object to add.
+   */
   public static addState(stateName: string, state: State) {
     this.states.set(stateName, state);
   }
 
+  /**
+   * Sets the current state of the game.
+   * @param stateName - The name of the state to set.
+   */
   public static setState(stateName: string) {
     const newState = this.states.get(stateName) as State;
 
@@ -63,6 +84,10 @@ export class Game {
     this.currentState.onInitialize();
   }
 
+  /**
+   * Returns the map containing all the players in the game.
+   * Creates a new map if it doesn't exist.
+   */
   public static getPlayers() {
     if (!this.players) {
       this.players = new Map<string, Player>();
@@ -71,6 +96,11 @@ export class Game {
     return this.players;
   }
 
+  /**
+   * Returns the player object associated with a websocket.
+   * @param websocket - The websocket to check.
+   * @returns - The player object associated with the websocket or undefined if not found.
+   */
   public static getPlayerFromWebsocket(websocket: WebSocket) {
     for (const player of this.players.values()) {
       if (player.getWebsocket() === websocket) {
